7bb0e51cfc0993bc05625197db29c789db96ed56
Tabita Barbosa <tabita.barbosa8@gmail.com>
2023-05-11 00:38:26 -0300

    Initial Commit

---
+++ TableViewExercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewExercise
+//
+//  Created by Tabita Barbosa on 11/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
